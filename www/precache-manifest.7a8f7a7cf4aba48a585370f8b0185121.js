43868f0c12629dbb06c42ba4bcccdc278a0ba5cc
Kevin Glover <ktg@cs.nott.ac.uk>
2019-01-21 14:50:37 +0000

    Include UI

---
+++ www/precache-manifest.7a8f7a7cf4aba48a585370f8b0185121.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "69cfe9926bba21a4575c",
+    "url": "css/app.d983c9ae.css"
+  },
+  {
+    "revision": "69cfe9926bba21a4575c",
+    "url": "js/app.4aa4f103.js"
+  },
+  {
+    "revision": "f037778efcc5eb42fe80",
+    "url": "js/chunk-2d230c8f.83a9c33a.js"
+  },
+  {
+    "revision": "afc06de82055c47045a3",
+    "url": "css/chunk-500556c0.8715a96e.css"
+  },
+  {
+    "revision": "afc06de82055c47045a3",
+    "url": "js/chunk-500556c0.15673e78.js"
+  },
+  {
+    "revision": "bdd2a902c7cce5e5a1a8",
+    "url": "js/chunk-vendors.eccac80a.js"
+  },
+  {
+    "revision": "ae097fbfb96b386f88a6",
+    "url": "css/group-settings.0d033446.css"
+  },
+  {
+    "revision": "ae097fbfb96b386f88a6",
+    "url": "js/group-settings.bbc1b14a.js"
+  },
+  {
+    "revision": "0b96bd47a6baf29baf3b",
+    "url": "css/group-store.cfda4ecf.css"
+  },
+  {
+    "revision": "0b96bd47a6baf29baf3b",
+    "url": "js/group-store.41ab4321.js"
+  },
+  {
+    "revision": "9fb10db61a40e76db2c0f5a4a16cae8d",
+    "url": "index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "cordova.js"
+  },
+  {
+    "revision": "658e66ecebe882701ad8664b07754ec8",
+    "url": "icons/apple-icon-152.png"
+  },
+  {
+    "revision": "3febef58312965ee4aa899f16e8bafb1",
+    "url": "icons/icon-192.png"
+  },
+  {
+    "revision": "39e5f7243d974d700293906afb9c6e38",
+    "url": "icons/icon-16.png"
+  },
+  {
+    "revision": "bce7a03e239515c1a56d1be9158ba596",
+    "url": "icons/full-icon-512.png"
+  },
+  {
+    "revision": "dc591d8cc4b0f0c8f9ab19abaa5f5f13",
+    "url": "icons/icon-32.png"
+  },
+  {
+    "revision": "bb03d203f3bd8d15c1aa49b3d8abb890",
+    "url": "icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b0d18220ad8aca7e7b9790f5dd97bbb3",
+    "url": "icons/icon-512.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
